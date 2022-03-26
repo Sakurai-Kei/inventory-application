80451bd887503a39e24972f75aacfefc0fcb9b38
R_Ashfell <luqmanulraiserhakim@gmail.com>
2022-03-27 03:14:45 +0800

    Create method has been implemented with validation

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,6 @@
 var async = require('async');
+const { body, validationResult } = require('express-validator');
+var Category = require('../models/categoryModel')
 var Item = require('../models/itemModel');
 
 
@@ -20,6 +22,103 @@ exports.itemDetail = function(req, res) {
         }
     }, function (err, results) {
         if(err) { return next(err) }
+        
         res.render('itemDetail', { item: results.item })
     })
 };
+
+exports.itemCreateGet = function(req, res) {
+    async.parallel({
+        categoryList: function(callback) {
+            Category.find().exec(callback)
+        }
+    }, function(err, results) {
+        if(err) { return next(err) }
+        let sortedCategoryList = results.categoryList;
+        sortedCategoryList.sort( function(a, b) {
+            if(a.name.trim().toUpperCase() < b.name.trim().toUpperCase()) {
+                return -1;
+            }
+            if(a.name.trim().toUpperCase() > b.name.trim().toUpperCase()) {
+                return 1;
+            }
+            return 0;
+        })
+        res.render('itemForm', { title: 'Create Item', categoryList: sortedCategoryList })
+    })
+}
+
+exports.itemCreatePost = [
+    function (req, res, next) {
+        if(!(req.body.category instanceof Array)) {
+            if(typeof req.body.category === 'undefined') {
+                req.body.category = [];
+            }
+            else {
+                req.body.category = new Array(req.body.category)
+            }
+        }
+        next();
+    },
+    body('name', 'Name must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('summary', 'Summary must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('category.*').escape(),
+    body('price', 'Price must not be empty').trim().isLength({ min: 1}).escape(),
+    body('price').custom( function(value, { req }) {
+        if(value < 0) {
+            throw new Error('Price must not be less than 0')
+        }
+        return true;
+    }),
+    body('stock', 'Quantity must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('stock').custom( function(value, { req }) {
+        if(value < 0) {
+            throw new Error('Quantity must not be less than 0')
+        }
+        return true;
+    }),
+    function(req, res, next) {
+        const errors = validationResult(req);
+
+        var item = new Item({
+            name: req.body.name,
+            summary: req.body.summary,
+            category: req.body.category,
+            price: req.body.price,
+            stock: req.body.stock
+        });
+
+        if(!errors.isEmpty()) {
+            async.parallel({
+                categoryList: function(callback) {
+                    Category.find().exec(callback)
+                }
+            }, function(err, results) {
+                if(err) { return next(err) }
+                results.categoryList.forEach( function(category) {
+                    if(item.category.indexOf(category._id) > -1) {
+                        category.checked='true';
+                    }
+                })
+                let sortedCategoryList = results.categoryList;
+                sortedCategoryList.sort( function(a, b) {
+                    if(a.name.trim().toUpperCase() < b.name.trim().toUpperCase()) {
+                        return -1;
+                    }
+                    if(a.name.trim().toUpperCase() > b.name.trim().toUpperCase()) {
+                        return 1;
+                    }
+                    return 0;
+                })        
+                res.render('itemForm', { title: 'Create Item', categoryList: sortedCategoryList, errors: errors.array() })
+            })
+            return;
+        }
+        else {
+            item.save( function(err) {
+                if(err) { return next(err) }
+                res.redirect(item.url);
+            })
+        }
+    }
+];
--- routes/item.js
+++ routes/item.js
@@ -5,6 +5,12 @@ var itemController = require('../controllers/itemController');
 
 router.get('/', itemController.itemList)
 
+router.get('/create', itemController.itemCreateGet);
+
+router.post('/create', itemController.itemCreatePost)
+
 router.get('/:id', itemController.itemDetail);
+
+
 
 module.exports = router;
